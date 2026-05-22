53a4bc07366f73916919a74dc62153ff7dae4b56
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2026-05-22 09:36:37 +0700

    fix: update getColor boundary to handle count=1

--- apps/dashboard/frontend/src/features/dashboard/utils/map-helpers.js
+++ apps/dashboard/frontend/src/features/dashboard/utils/map-helpers.js
@@ -3,7 +3,7 @@ export function getColor(count) {
         case count > 109: return '#800026';
         case count > 73:  return '#BD0026';
         case count > 37:  return '#E31A1C';
-        case count > 1:   return '#FC4E2A';
+        case count > 0:   return '#FC4E2A';
         default:          return '#FD8D3C';
     }
 }
