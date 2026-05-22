582af86d33b60ff543f0624e8d9f08dda5ef758a
Mohammed Lazuardi Yasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2026-05-22 10:34:49 +0700

    fix: remove duplicate imports in main.js (#20)

--- apps/dashboard/frontend/src/main.js
+++ apps/dashboard/frontend/src/main.js
@@ -1,7 +1,5 @@
 import 'leaflet/dist/leaflet.css';
 import './style.css';
-import 'leaflet/dist/leaflet.css';
-import './style.css';
 import mockSecurityData from './testing/mocks/mockSecurityData.json';
 import { initDashboard } from './features/dashboard/components/dashboard-view.js';
 
